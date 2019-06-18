ae734b760b6c6bab19144a624fca2531c17b005a
Alexis Duarte <alexis.duarte@hotmail.com>
2019-06-18 01:14:50 -0700

    Added my curriculum vitae

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Home.css';
 import me from '../../assets/images/me.jpg';
 
@@ -12,10 +12,17 @@ class Home extends Component {
                 <div className="home_information">
                     <h1>I'm Alexis.</h1>
                     <p>A <code>Computer Systems Engineer</code> with a passion for web and mobile development.</p>
-                    <p>In here you'll find some information about me and also some projects I've worked and/or still working on.</p>
+                    <p>
+                        In here you'll find some information about me and also some projects I've worked and/or still working on
+                        or you could just get my <button className="downloadButton code" onClick={this.downloadCV}>Curriculum Vitae</button> in PDF format
+                    </p>
                 </div>
             </div>
         );
+    }
+
+    downloadCV() {
+        window.open(process.env.PUBLIC_URL + 'Alexis Duarte - CV.pdf');
     }
 }
 
