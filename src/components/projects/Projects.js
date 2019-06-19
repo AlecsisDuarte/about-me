4120769196752a965b8753091506c08a0d8251cc
Alexis Duarte <alexis.duarte@hotmail.com>
2019-06-18 18:16:41 -0700

    Finished the projects container

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,17 +1,50 @@
 import React, {Component} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCog } from '@fortawesome/free-solid-svg-icons';
+import Project from '../project/Project';
 import './Projects.css';
 
 class Projects extends Component {
+
+    projects = [
+        {
+            name: 'Products List',
+            github: 'https://github.com/AlecsisDuarte/flutter_course',
+            playstore: 'https://play.google.com/store/apps/details?id=com.duarte.fluttercourse',
+            url: null,
+            description: 'Application that allows you to publish your products and also view other users products with their price and position',
+            status: 'stalled',
+        },
+        {
+            name: 'Oky',
+            github: 'https://github.com/AlecsisDuarte/Oki',
+            playstore: null,
+            url: null,
+            description: 'Drowsiness prevental android application',
+            status: 'stalled',
+        },
+        {
+            name: 'About Me',
+            github: 'https://github.com/AlecsisDuarte/about-me',
+            playstore: null,
+            url: 'https://about-me.alecsisduarte.now.sh',
+            description: 'React web page (this actual web page) created to showcase some of my projects and to present myself',
+            status: 'active',
+        }
+    ];
+
     render() {
         return (
-            <div className="projects_wrapper">
-                <p className="projects_title">Work in progress</p>
-                <p className="projects_message">I'll be working on this, just hang on</p>
-                <p className="projects_icon">
-                    <FontAwesomeIcon className="fa-spin" icon={faCog} />
-                </p>
+            <div className="projects_page">
+                
+                <div className="projects_wrapper">
+                    {
+                        this.projects.map((project, index) =>
+                            <Project key={index} title={project.name} description={project.description}
+                                github={project.github} playstore={project.playstore} 
+                                url={project.url} status={project.status}/>
+                        )
+                    }
+                </div>
+                <p>For more up-to-date projects, you could visit my <a href="https://github.com/AlecsisDuarte?tab=repositories" className="inline_button">Github repositories</a></p>
             </div>
         );
     }
